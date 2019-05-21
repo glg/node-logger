17499c1d77a2539cca73e994fadfb42844b626a3
Cedric Wong <hkcedricw@gmail.com>
2019-05-21 10:41:09 +0800

    add format.splat(), so it supports formatting

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ module.exports = {
                     format: 'YYYY-MM-DD HH:mm:ss'
                 }),
                 winston.format.errors({ stack: true }),
+                winston.format.splat(),
                 winston.format.simple()
             ),
             defaultMeta: metadata,
