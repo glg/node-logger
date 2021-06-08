76b11d48c2990340466b8690aca89dc9f8af87f7
Phred Lane <plane@glgroup.com>
2021-06-08 13:36:25 -0500

    fixed spacing

--- index.js
+++ index.js
@@ -18,27 +18,27 @@ const levels = validLogLevels.reduce((result, level, index) => {
 
 module.exports = {
   createLogger: (logLevel='info', metadata={}, stderrLevels=[], { format } = {}) => {
-        return winston.createLogger({
-            format: format || winston.format.combine(
-                winston.format.timestamp({
-                    format: 'YYYY-MM-DD HH:mm:ss'
-                }),
-                winston.format.errors({ stack: true }),
-                winston.format.splat(),
-                winston.format.simple()
-            ),
-            defaultMeta: metadata,
-            level: logLevel,
-            levels,
-            transports: [
-                new winston.transports.Console({
-                    handleExceptions: true,
-                    stderrLevels,
-                })
-            ]
-        });
-    },
-    format: winston.format,
-    levels,
-    validLogLevels
+    return winston.createLogger({
+      format: format || winston.format.combine(
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.simple()
+      ),
+      defaultMeta: metadata,
+      level: logLevel,
+      levels,
+      transports: [
+        new winston.transports.Console({
+          handleExceptions: true,
+          stderrLevels,
+        })
+      ]
+    });
+  },
+  format: winston.format,
+  levels,
+  validLogLevels
 };
